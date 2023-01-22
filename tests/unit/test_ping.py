b424176002903f23f0b6d7e36d900324c287e243
MagdalSaturs <97666941+MagdalSaturs@users.noreply.github.com>
2023-01-22 14:27:47 +0100

    Update test_ping.py

--- tests/unit/test_ping.py
+++ tests/unit/test_ping.py
@@ -25,4 +25,26 @@ def test_update_user_returns_501_response_put():
     assert result.status_code == 501
 
 
+def test_users_returns_501_response_get():
+    result = users()
+    assert result.status_code == 501
 
+def test_create_user_returns_user():
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
+    with app.test_request_context(method="POST", path="/users", json=payload):
+        result = create_user()
+    assert result.json == payload
+    
+def test_update_user_returns_user():
+    payload = {"first_name": "Tosia", "last_name": "Saturska"}
+    with app.test_request_context(method="PUT", path="/users/1", json=payload):
+        result = update_user()
+    assert result.json == payload
+    
+def test_user_returns_200_response_patch():
+    result = user(1)
+    assert result.status_code == 200
+    
+def test_delete_user_returns_204_response_delete():
+    result = delete_user(1)
+    assert result.status_code == 204
