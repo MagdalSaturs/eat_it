67dfaa5344a40be8d54de5bd8f91c5796f3a8eea
MagdalSaturs <97666941+MagdalSaturs@users.noreply.github.com>
2023-02-11 14:39:43 +0100

    Update app.py

--- eat_it/app.py
+++ eat_it/app.py
@@ -1,5 +1,17 @@
 from flask import Flask, request
+from werkzeug.exceptions import HTTPException
 app = Flask(__name__)
+
+#obsługa wyjątku NotImplementedError
+@app.errorhandler(NotImplementedError)
+def handle_not_implemented_error(e):
+    return '501 Not Implemented', 501
+
+@app.errorhandler(HTTPException)
+def handle_http_exception(e):
+    return e.description, e.code
+#Koniec obsługi wyjątku
+
 
 @app.route('/users', methods=['GET'])
 def get_users():
@@ -12,14 +24,17 @@ def create_user():
 
 @app.route('/users/<id>', methods=['PUT'])
 def update_user(id):
+    raise NotImplementedError
     user_data = request.get_json()
     return user_data, 200
 
 @app.route('/users/<id>', methods=['PATCH'])
 def partially_update_user(id):
+    raise NotImplementedError
     user_data = request.get_json()
     return user_data, 200
 
 @app.route('/users/<id>', methods=['DELETE'])
 def delete_user(id):
+    raise NotImplementedError
     return '', 204
