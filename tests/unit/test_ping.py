6d3c49f066025806e67c1e7b7c38b6b4478291d3
MagdalSaturs <97666941+MagdalSaturs@users.noreply.github.com>
2023-02-11 14:31:34 +0100

    Update test_ping.py

--- tests/unit/test_ping.py
+++ tests/unit/test_ping.py
@@ -1,31 +1,27 @@
 from eat_it.app import ping, users, user, create_user, delete_user, update_user
 
 
-class EndpointTests(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
+def test_get_users(self):
+    response = self.app.get('/users')
+    self.assertEqual(response.status_code, 501)
+    self.assertEqual(response.data, b'501 Not Implemented')
 
-    def test_get_users(self):
-        response = self.app.get('/users')
-        self.assertEqual(response.status_code, 501)
-        self.assertEqual(response.data, b'501 Not Implemented')
+def test_create_user(self):
+    response = self.app.post('/users', json={'username': 'john'})
+    self.assertEqual(response.status_code, 201)
+    self.assertEqual(response.get_json(), {'username': 'john'})
 
-    def test_create_user(self):
-        response = self.app.post('/users', json={'username': 'john'})
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.get_json(), {'username': 'john'})
+def test_update_user(self):
+    response = self.app.put('/users/1', json={'username': 'john'})
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.get_json(), {'username': 'john'})
 
-    def test_update_user(self):
-        response = self.app.put('/users/1', json={'username': 'john'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), {'username': 'john'})
+def test_partially_update_user(self):
+    response = self.app.patch('/users/1', json={'username': 'john'})
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.get_json(), {'username': 'john'})
 
-    def test_partially_update_user(self):
-        response = self.app.patch('/users/1', json={'username': 'john'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), {'username': 'john'})
-
-    def test_delete_user(self):
-        response = self.app.delete('/users/1')
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(response.data, b'')
+def test_delete_user(self):
+    response = self.app.delete('/users/1')
+    self.assertEqual(response.status_code, 204)
+    self.assertEqual(response.data, b'')
