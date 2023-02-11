3d946ada4a72b40cddbbb3b9ae4ba9a14423032e
MagdalSaturs <97666941+MagdalSaturs@users.noreply.github.com>
2023-02-11 14:29:18 +0100

    Update app.py

--- eat_it/app.py
+++ eat_it/app.py
@@ -1,49 +1,25 @@
-from flask import Flask, Response
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.get('/ping')
-def ping():
-    return Response(status = 501)
+@app.route('/users', methods=['GET'])
+def get_users():
+    return '501 Not Implemented', 501
 
-@app.get('/users')
-def users():
-    return Response(status = 501)
-
-@app.patch('/users/<user_id>')
-def user(user_id):
-    return Response(status = 501)
-
-@app.post('/users')
+@app.route('/users', methods=['POST'])
 def create_user():
-    return Response(status = 501)
+    user_data = request.get_json()
+    return user_data, 201
 
-@app.delete('/users/<user_id>')
-def delete_user(user_id):
-    return Response(status = 501)
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    user_data = request.get_json()
+    return user_data, 200
 
-@app.put('/users/<user_id>')
-def update_user(user_id):
-    return Response(status = 501)
+@app.route('/users/<id>', methods=['PATCH'])
+def partially_update_user(id):
+    user_data = request.get_json()
+    return user_data, 200
 
-@app.get('/users')
-def users():
-    return Response(status = 501)
-
-@app.post('/users')
-def create_user():
-    user = request.json
-    return jsonify(user)
-
-@app.put('/users/<id>')
-def update_user(user_id):
-    user = request.json
-    return jsonify(user)
-
-@app.patch('/users/<id>')
-def user(user_id):
-    user = request.json
-    return jsonify(user)
-
-@app.delete('/users/<user_id>')
-def delete_user(user_id):
-    return Response(status = 204)
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    return '', 204
