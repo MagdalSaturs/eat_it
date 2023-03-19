0d3b32c285350a3417116138ffb4e800923c3b6b
magdalena <u31_magsat_waw@technischools.com>
2023-03-19 14:37:19 +0100

    poprawa Do domu: naprawienie błędów

--- eat_it/app.py
+++ eat_it/app.py
@@ -1,33 +1,36 @@
-from flask import Flask, Response
+from urllib import request
+
+from flask import Flask, Response, jsonify
+
 app = Flask(__name__)
 
 @app.get('/ping')
 def ping():
-    return Response(status = 501)
+    return Response(status=501)
 
 @app.get('/users')
 def users():
-    return Response(status = 501)
+    return Response(status=501)
 
 @app.patch('/users/<user_id>')
 def user(user_id):
-    return Response(status = 501)
+    return Response(status=501)
 
 @app.post('/users')
 def create_user():
-    return Response(status = 501)
+    return Response(status=501)
 
 @app.delete('/users/<user_id>')
 def delete_user(user_id):
-    return Response(status = 501)
+    return Response(status=501)
 
 @app.put('/users/<user_id>')
 def update_user(user_id):
-    return Response(status = 501)
+    return Response(status=501)
 
 @app.get('/users')
 def users():
-    return Response(status = 501)
+    return Response(status=501)
 
 @app.post('/users')
 def create_user():
@@ -46,4 +49,4 @@ def user(user_id):
 
 @app.delete('/users/<user_id>')
 def delete_user(user_id):
-    return Response(status = 204)
+    return Response(status=204)
--- tests/integrations/test_app.py
+++ tests/integrations/test_app.py
@@ -1,5 +1,6 @@
 from eat_it.app import app
 
+
 def test_app_has_ping_endpoint():
     with app.test_client() as client:
         response = client.get('/ping')
@@ -56,7 +57,7 @@ def test_app_user_create_endpoint():
 def test_app_user_update_endpoint():
     payload = {"first_name": "Tosia", "last_name": "Saturska"}
     client = app.test_client()
-    response = client.putth='/users/1, json=payload)
+    response = client.put(path='/users/1', json=payload)
     assert response.status_code == 200
     
 def test_app_has_users_endpoint_patch():
