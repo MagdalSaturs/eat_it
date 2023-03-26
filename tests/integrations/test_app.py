1b9c70f99f7d5cb7e3d841f2747a177684d3c7b5
MagdalSaturs <97666941+MagdalSaturs@users.noreply.github.com>
2023-03-26 10:28:16 +0200

    Update test_app.py

--- tests/integrations/test_app.py
+++ tests/integrations/test_app.py
@@ -1,71 +1,59 @@
 from eat_it.app import app
 
-
-def test_app_has_ping_endpoint():
-    with app.test_client() as client:
-        response = client.get('/ping')
-        assert response.status_code == 501
+UNIMPLEMENTED = 501
 
 
-def test_app_has_users_endpoint_get():
-    with app.test_client() as client:
-        response = client.get('/users')
-        assert response.status_code == 501
+def test_app_has_ping_endpoint() -> None:
+    client = app.test_client()
+    response = client.get(path="/ping")
+    assert response.status_code == UNIMPLEMENTED
 
 
-def test_app_has_users_endpoint_patch():
-    with app.test_client() as client:
-        response = client.patch('/users/1')
-        assert response.status_code == 501
-
-
-def test_app_has_users_endpoint_post():
-    with app.test_client() as client:
-        response = client.post('/users')
-        assert response.status_code == 501
-
-
-def test_app_has_users_endpoint_delete():
-    with app.test_client() as client:
-        response = client.delete('/users/1')
-        assert response.status_code == 501
-
-
-def test_app_has_user_endpoint_post():
-    with app.test_client() as client:
-        response = client.post('/users')
-        assert response.status_code == 501
-
-
-def test_app_has_user_endpoint_put():
-    with app.test_client() as client:
-        response = client.put('/users/1')
-        assert response.status_code == 501
-        
-
-def test_app_has_users_endpoint_get():
-    with app.test_client() as client:
-        response = client.get('/users')
-        assert response.status_code == 501
-        
-def test_app_user_create_endpoint():
+def test_app_user_create_endpoint() -> None:
     payload = {"first_name": "Magda", "last_name": "Saturska"}
     client = app.test_client()
     response = client.post(path='/users', json=payload)
-    assert response.status_code == 201
+    assert response.status_code == 200
 
-def test_app_user_update_endpoint():
-    payload = {"first_name": "Tosia", "last_name": "Saturska"}
+
+def test_app_user_update_endpoint() -> None:
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
     client = app.test_client()
-    response = client.put(path='/users/1', json=payload)
+    response = client.put(path='/users', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_get_endpoint() -> None:
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
+    client = app.test_client()
+    response = client.get(path='/users/<id>', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_delete_endpoint() -> None:
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
+    client = app.test_client()
+    response = client.delete(path='/users/<id>', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_get_endpoint() -> None:
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
+    client = app.test_client()
+    response = client.get(path='/users/<id>', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_delete_endpoint() -> None:
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
+    client = app.test_client()
+    response = client.delete(path='/users/<id>', json=payload)
+    assert response.status_code == 200
+
+def test_app_user_patch_endpoint() -> None:
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
+    client = app.test_client()
+    response = client.patch(path='/users/<id>', json=payload)
     assert response.status_code == 200
     
-def test_app_has_users_endpoint_patch():
-    with app.test_client() as client:
-        response = client.patch('/users/1')
-        assert response.status_code == 200
-        
-def test_app_has_users_endpoint_delete():
-    with app.test_client() as client:
-        response = client.delete('/users/1')
-        assert response.status_code == 204
+def test_app_users_get_endpoint() -> None:
+    payload = {"first_name": "Magda", "last_name": "Saturska"}
+    client = app.test_client()
+    response = client.get(path='/users/<id>', json=payload)
+    assert response.status_code == 501
