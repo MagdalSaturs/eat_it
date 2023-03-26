3a34e1cb1b0aa26f8f6170dfc24be3e4a1fada98
MagdalSaturs <97666941+MagdalSaturs@users.noreply.github.com>
2023-03-26 10:32:08 +0200

    Update test_users.py

--- tests/unit/test_users.py
+++ tests/unit/test_users.py
@@ -1,7 +1,8 @@
-import pytest
-from eat_it.app import create_user, update_user, app
 import json
 
+import pytest
+
+from eat_it.app import app, create_user, update_user, get_user, delete_user, get_user
 
 @pytest.fixture
 def payload() -> dict:
@@ -18,7 +19,7 @@ def test_create_user_prints_user_on_console(payload: dict, capsys) -> None:
     with app.test_request_context(method="POST", path="/users", json=payload):
         create_user()
     actual = capsys.readouterr().out
-    expected = f"{json.dumps(payload)}\n".replace('"', '')
+    expected = f"{json.dumps(payload)}\n".replace('"', "'")
     assert actual == expected
 
 
@@ -28,3 +29,51 @@ def test_update_user_returns_user(payload: dict) -> None:
     assert result.json == payload
 
 
+def test_update_user_prints_user_on_console(payload: dict, capsys) -> None:
+    with app.test_request_context(method="PUT", path="/users", json=payload):
+        update_user()
+    actual = capsys.readouterr().out
+    expected = f"{json.dumps(payload)}\n".replace('"', "'")
+    assert actual == expected
+
+
+def test_get_user_returns_user(payload: dict) -> None:
+    with app.test_request_context(method="GET", path="/users"):
+        result = get_user()
+    assert result.json == payload
+
+
+def test_get_user_prints_user_on_console(payload: dict, capsys) -> None:
+    with app.test_request_context(method="GET", path="/users"):
+        get_user()
+    actual = capsys.readouterr().out
+    expected = f"{json.dumps(payload)}\n".replace('"', "'")
+    assert actual == expected
+
+
+def test_delete_user_returns_user(payload: dict) -> None:
+    with app.test_request_context(method="DELETE", path="/users"):
+        result = delete_user()
+    assert result.json == payload
+
+
+def test_delete_user_prints_user_on_console(payload: dict, capsys) -> None:
+    with app.test_request_context(method="DELETE", path="/users"):
+        delete_user()
+    actual = capsys.readouterr().out
+    expected = f"{json.dumps(payload)}\n".replace('"', "'")
+    assert actual == expected
+
+
+def test_get_users_returns_users(payload: dict) -> None:
+    with app.test_request_context(method="GET", path="/users"):
+        result = get_user()
+    assert result.json == payload
+
+
+def test_get_users_prints_users_on_console(payload: dict, capsys) -> None:
+    with app.test_request_context(method="GET", path="/users"):
+        get_user()
+    actual = capsys.readouterr().out
+    expected = f"{json.dumps(payload)}\n".replace('"', "'")
+    assert actual == expected
